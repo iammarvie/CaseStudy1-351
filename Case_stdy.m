29ab2a7ebcf97f6622b94f27b80f25343b88d65d
Alchemy07 <hernandez.j@wustl.edu>
2023-03-05 17:08:47 -0600

    work

--- Case_stdy.m
+++ Case_stdy.m
@@ -16,33 +16,34 @@ delta_t = 1/fs;
 t_naught = 2*tau;
 t = [0:delta_t:t_end];
 
-%Bands
-band1 = [20 200]; %20hz to 200hz
-band2 = [200 2000]; %200hz to 2khz
-band3 = [2000 10000]; %2khz to 10khz
-band4 = [10000 20000]; %10khz to 20khz
-band5 = [20000 20000]; %20khz
+% Bands
+band1 = [20 200]; % 20hz to 200hz
+band2 = [200 2000]; % 200hz to 2khz
+band3 = [2000 10000]; % 2khz to 10khz
+band4 = [10000 20000]; % 10khz to 20khz
+band5 = [20000 20000]; % 20khz
 
-%Center frequencies sqrt(f1*f2)
+% Center frequencies sqrt(f1*f2)
 center1 = sqrt(20*200);
 center2 = sqrt(200*2000);
 center3 = sqrt(2000*10000);
 center4 = sqrt(10000*20000);
 center5 = sqrt (20000*20000);
 
-%filter coefficients
+% Filter Coefficients
 ahigh = [1 ((1/fs)/tau)-1];
 bhigh = [1 -1];
 blow = [0 ((1/fs)/tau)];
 alow = [1 ((1/fs)/tau)-1];
 
 
-%HIGH PASS
-input = zeros(1,length(t)); %
+% HIGH PASS FILTER 
+input = zeros(1,length(t)); 
 input(1) = 1;
+
 yhigh_fil = filter(bhigh,ahigh,input); %filter
 
-%LOW PASS
+% LOW PASS FILTER
 ylow_fil = filter(blow,alow,input);
 
 %% TREBLE BOOST (HIGH PASS FILTER)
