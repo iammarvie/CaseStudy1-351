d021f0759cf97943ed76eff6f98382d26008bdcf
iammarvie <67104755+iammarvie@users.noreply.github.com>
2023-03-05 18:16:18 -0600

    Finished the giant mod

--- Case_study_scratch.m
+++ Case_study_scratch.m
@@ -15,30 +15,36 @@
 R = 1000;      % Ohms
 C = 5 * 10^-6 ;  % Farads
 tau = R * C;       % seconds
-fsample = 44100;         % sample frequency = 44.1 kHz
-delta_time = 1/fsample;     % sampling period = 1/sample frequency
+fsound = 44100;         % sample frequency = 44.1 kHz
+delta_time = 1/fsound;     % sampling period = 1/sample frequency
 
 % Bands
 band1 = [20 200]; % 20hz to 200hz
-band2 = [200 2000]; % 200hz to 2khz
-band3 = [2000 10000]; % 2khz to 10khz
-band4 = [10000 20000]; % 10khz to 20khz
-band5 = [20000 20000]; % 20khz
+band2 = [200 500]; % 200hz to 2khz
+band3 = [900 1000]; % 2khz to 10khz
+band4 = [2000 5000]; % 10khz to 20khz
+band5 = [10000 20000]; % 20khz
 
 % Center frequencies sqrt(f1*f2)
-center1 = sqrt(20*200);
-center2 = sqrt(200*2000);
+center1 = sqrt(20*200); %%low
+%mid
+center2 = sqrt(200*2000); 
 center3 = sqrt(2000*10000);
 center4 = sqrt(10000*20000);
-center5 = sqrt (20000*20000);
-
-% Set up Step Input:
-t1 = 0:delta_time:25/center1;
-t2 = 0:delta_time:25/center2;
-
-% Input Complex Exponential:
-
-% Filter @ 10 Hz:
-LowpassFilter10Hz = lsim([1/tau], [1 1/tau], ce_1, t1);
-HighpassFilter10Hz = lsim([1 0], [1 1/tau], ce_1, t1);
-
+%
+center5 = sqrt (20000*20000); %% High
+gain = [-15,-7,0,7,15];
+%% GIANT STEPS
+input = giant;
+%% TREBLE BOOST (HIGH PASS FILTER)
+[Band1,filt1] = highpass(input,10000,fsound);
+%% UNITY (BAND PASS FILTER)
+[Band2,filt2] = bandpass(input,band2,fsound);
+[Band3,filt3] = bandpass(input,band3,fsound);
+[Band4,filt4] = bandpass(input,band4,fsound);
+%% BASS BOOST (LOW PASS FILTER)
+%LowpassFilter = lsim([1/tau], [1 1/tau], input, t1);
+[Band5,filt5] = lowpass(input,200,fsound);
+%% combined filters
+Mixer_giant = gain(1)*Band1+gain(2)*Band2+gain(3)*Band3+gain(4)*Band4+gain(5)*Band5;
+sound(Mixer_giant);
